4d1c4fb26b745c0ba5183335c998fa7036bb53e9
Hemanth <hemanthbisai7089@gmail.com>
2026-09-26 13:47:56 +0530

    5th step also working, modified scannerPositon function to make it work Properly

--- scanning_for_Particles/sketch.js
+++ scanning_for_Particles/sketch.js
@@ -12,20 +12,24 @@ function setup() {
     r.InitWindow(WIDTH, HEIGHT, "scanning for particles");
     r.SetTargetFPS(60);
 }
+const detectorOneStart = 0;
+let detectorOneX = detectorOneStart;
+let detectorOneY = 0;
+const detectorOneWidth = 20;
+let detectorOneColor = r.WHITE;
+const detectorOneEnd = WIDTH / 2;
+let detectorOneReached = false;
+let oneReached = false;
 
-let scannerX = 0;
-let scannerY = 0;
-const scannerWidth = 20;
-let color = r.WHITE;
-let scannerReached = false;
+const detectorTwoStart = WIDTH / 2 + 1;
+let detectorTwoX = detectorTwoStart;
+let detectorTwoY = 0;
+const detectorTwoWidth = 20;
+const detectorTwoEnd = WIDTH;
+let detectorTwoColor = r.WHITE;
+let detectorTwoReached = false;
+let twoReached = false;
 
-function scannerPosition() {
-    if (scannerX + scannerWidth === WIDTH) {
-        scannerReached = true;
-    } else if (scannerX === 0) {
-        scannerReached = false;
-    }
-}
 
 const particle1X = 100;
 const particle1Y = 0;
@@ -36,26 +40,55 @@ const particle2Y = 0;
 const Particle2Width = 5;
 const particle2End = particle2X + Particle2Width;
 
+function scannerPosition(detectorX, detectorWidth, detectorEnd, detectorNumber) {
+    if (detectorNumber === 1) {
+        if (detectorX + detectorWidth >= detectorEnd) {
+            oneReached = true;
+        } else if (detectorX <= 0) {
+            oneReached = false;
+
+        } return oneReached;
+    }
+    if (detectorNumber === 2) {
+        if (detectorX + detectorWidth >= detectorEnd) {
+            twoReached = true;
+        } else if (detectorX <= detectorTwoStart) {
+            twoReached = false;
+
+        } return twoReached;
+    }
+}
 
 function update() {
 
-    scannerPosition();
-    color = overlapCheck(particle1X, particle1End, particle2X, particle2End);
+    detectorOneReached = scannerPosition(detectorOneX, detectorOneWidth, detectorOneEnd, 1);
+    detectorTwoReached = scannerPosition(detectorTwoX, detectorTwoWidth, detectorTwoEnd, 2);
 
+    detectorOneColor = overlapCheck(particle1X, particle1End, particle2X, particle2End, detectorOneX);
+    detectorTwoColor = overlapCheck(particle1X, particle1End, particle2X, particle2End, detectorTwoX);
 
-    if (scannerReached) {
+    if (detectorOneReached) {
 
-        scannerX -= 1;
+        detectorOneX -= 1;
+
     } else {
-        scannerX += 1;
+        detectorOneX += 1;
     }
+    if (detectorTwoReached) {
+
+        detectorTwoX -= 3;
+
+    } else {
+        detectorTwoX += 3;
+    }
+
 
 
 }
 
-function overlapCheck(particle1X, particle1End, particle2X, particle2End) {
-    const atParticle1 = scannerX + scannerWidth >= particle1X && scannerX <= particle1End;
-    const atparticle2 = scannerX + scannerWidth >= particle2X && scannerX <= particle2End;
+function overlapCheck(particle1X, particle1End, particle2X, particle2End, detectorX) {
+    const atParticle1 = detectorX + detectorOneWidth >= particle1X && detectorX <= particle1End;
+    const atparticle2 = detectorX + detectorOneWidth >= particle2X && detectorX <= particle2End;
 
     if (atParticle1 || atparticle2) return r.Fade(r.RED, 0.7);
     else {
@@ -71,7 +104,9 @@ function draw() {
 
     r.DrawRectangle(particle2X, particle2Y, Particle2Width, HEIGHT, r.BLUE);
     r.DrawRectangle(particle1X, particle1Y, Particle1Width, HEIGHT, r.BLUE);
-    r.DrawRectangle(scannerX, scannerY, scannerWidth, HEIGHT, color);
+    r.DrawRectangle(detectorOneX, detectorOneY, detectorOneWidth, HEIGHT, detectorOneColor);
+    r.DrawRectangle(detectorTwoX, detectorTwoY, detectorTwoWidth, HEIGHT, detectorTwoColor);
+
 
     r.EndDrawing();
 }
